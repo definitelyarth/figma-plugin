3d57ba80a97f8e61196bb37d68aeb115c067009e
Arth <arth.thapa@rocketium.com>
2024-10-24 13:36:01 +0530

    gzip download

--- src/ui/utils/download.ts
+++ src/ui/utils/download.ts
@@ -1,13 +1,13 @@
 import { RocketiumPortableFormat } from "rocketium-types";
 
 const downloadRktm = async (rocketiumDocument: RocketiumPortableFormat) => {
-  // const brotli = await brotliPromise;
-
-  // const encoder = new TextEncoder();
+  const cs = new CompressionStream("gzip");
   const jsonString = JSON.stringify(rocketiumDocument);
-  // const data = brotli.compress(encoder.encode(jsonString));
+  const blob = new Blob([jsonString]);
+  const compressedStream = blob.stream().pipeThrough(cs);
+  const res = await new Response(compressedStream).blob();
 
-  const blobURL = window.URL.createObjectURL(new Blob([jsonString]));
+  const blobURL = window.URL.createObjectURL(res);
   const link = document.createElement("a");
   link.href = blobURL;
   link.download = `${rocketiumDocument.metadata.name}.rktm`;
